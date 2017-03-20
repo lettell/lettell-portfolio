55c739eef80b094d6e22cb9eb6b77b47a68ea15d
Paulius Jarosius <jarosius@live.com>
2017-03-20 15:37:04 +0200

    Created seeds file for sample data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,24 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+10.times do |blog|
+  Blog.create(
+  title: "My Blog Post #{blog}",
+  body: "But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure?"
+  )
+end
+puts "10 blog posts created"
+5.times do |skill|
+  Skill.create(
+  title: "rails #{skill}",
+  percent_utilized: 15
+  )
+end
+ puts "5 skill created"
+9.times do |potfolio_item|
+  Portfolio.create(
+  title: "Portfolio title: #{potfolio_item}",
+  subtitle: "My great service",
+  body: "But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness.",
+  main_image: "https://placehold.it/600x400",
+  thumb_image: "https://placehold.it/350x200"
+  )
+end
+  puts "9 portfolio created"
