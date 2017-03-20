f932ff5e44a865ebc2473441c18ba1e5fce6cbfc
Paulius Jarosius <jarosius@live.com>
2017-03-20 16:23:24 +0200

    Integrated new and create action

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,2 +1,25 @@
 class PortfoliosController < ApplicationController
+  def index
+    @portfolio_items = Portfolio.all
+  end
+  def new
+    @Portfolio_item = Portfolio.new
+  end
+  def create
+    @Portfolio_item = Portfolio.new(portfolio_params)
+
+    respond_to do |format|
+      if @Portfolio_item.save
+        format.html { redirect_to portfolios_path, notice: 'Blog was successfully created.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  private
+
+  def portfolio_params
+    params.require(:portfolio).permit(:title, :subtitle, :body)
+  end
 end
