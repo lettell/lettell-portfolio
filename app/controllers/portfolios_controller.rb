67c27c0f934ac4087d7f0027392348c58fb55024
Paulius Jarosius <jarosius@live.com>
2017-03-20 17:25:31 +0200

    destroy  action

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,11 +1,16 @@
 class PortfoliosController < ApplicationController
   before_action :set_portfolio, only: [:show, :edit, :update, :destroy]
+
   def index
     @portfolio_items = Portfolio.all
   end
+
+  def show; end
+
   def new
     @Portfolio_item = Portfolio.new
   end
+
   def create
     @portfolio_item = Portfolio.new(portfolio_params)
 
@@ -17,23 +22,30 @@ class PortfoliosController < ApplicationController
       end
     end
   end
-  def edit
-  end
+
+  def edit; end
+
   def update
     respond_to do |format|
       if @portfolio_item.update(portfolio_params)
-        format.html { redirect_to portfolios_path, notice: 'The record was seccesfully updated.'}
+        format.html { redirect_to portfolios_path, notice: 'The record was seccesfully updated.' }
       else
         format.html { render :edit }
       end
     end
   end
-
+  def destroy
+    @portfolio_item.destroy
+    respond_to do |format|
+      format.html { redirect_to portfolios_url, notice: 'The record deleted' }
+    end
+  end
   private
 
   def portfolio_params
     params.require(:portfolio).permit(:title, :subtitle, :body)
   end
+
   def set_portfolio
     @portfolio_item = Portfolio.find(params[:id])
   end
