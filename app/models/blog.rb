930659b705f01ee93395986ef2a88bf03ee4a83b
Paulius Jarosius <jarosius@live.com>
2017-03-20 18:19:30 +0200

    add friendly routes

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,2 +1,10 @@
 class Blog < ApplicationRecord
+  extend FriendlyId
+  friendly_id :slug_candidates, use: :slugged
+  def slug_candidates
+    [
+      :title,
+      [:title, :created_at]
+    ]
+  end
 end
