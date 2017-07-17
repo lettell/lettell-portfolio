4799d11f11b8e3a8cbe11ea18c7d9382a9bdf1cf
Paulius Jarosius <jarosius@live.com>
2017-07-17 20:16:52 +0300

    Implemented redis libary and created comment generator

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -6,10 +6,21 @@ class Blog < ApplicationRecord
   validates_presence_of :title, :body
 
   belongs_to :topic
+
+  has_many :comments, dependent: :destroy
+
   def slug_candidates
     [
       :title,
       [:title, :created_at]
     ]
+  end
+  
+  def self.special_blogs
+    all
+  end
+
+  def self.featured_blogs
+    limit(2)
   end
 end
